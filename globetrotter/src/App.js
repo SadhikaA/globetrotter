b3a8d7c7efa85687789b1ebda5c1b4aaff6a3146
Sadhika Akula <sadhika.akula@berkeley.edu>
2023-06-03 13:28:34 -0700

    chakra and basic ui

--- globetrotter/src/App.js
+++ globetrotter/src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
+import { Text, Input, Select } from '@chakra-ui/react';
+import { FormControl, FormLabel, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>🌎 Globetrotter</h1>
-      <p class="info">subheading goes here</p>
-      <p class="headers">Upload data: </p>
+      <Text
+        fontSize='5xl'
+        fontWeight='bold'
+        color='#1c6ca6ff'
+      >  globetrotter
+      </Text>
+      <p class="info">your AI-travel planner</p>
+      <FormControl>
+        <FormLabel>First name</FormLabel>
+        <Input placeholder='First name' />
+        <FormLabel>Country</FormLabel>
+        <Select placeholder='Select country'>
+          <option>United Arab Emirates</option>
+          <option>Nigeria</option>
+        </Select>
+        <FormLabel>Amount</FormLabel>
+        <NumberInput max={50} min={10}>
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </FormControl>
     </div>
   );
 }
