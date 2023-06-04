9a81fa2c3a38365eb177b58a119322a118d98574
Sadhika Akula <sadhika.akula@berkeley.edu>
2023-06-04 00:16:01 -0700

    complete page 1

--- globetrotter/src/Home.js
+++ globetrotter/src/Home.js
@@ -1,24 +1,48 @@
 import { Text, Button } from "@chakra-ui/react";
 import "./Home.css";
-import { Link } from '@chakra-ui/react'
+import { Link } from "@chakra-ui/react";
 import { Link as NavLink } from "react-router-dom";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoic2FrdWxhOCIsImEiOiJjbGlnemVpc2sxbHRzM2ZtbXNpZHAxY3VuIn0.SL0bzKkz-T_huKrdf5Doaw";
 
 function Home() {
-    return (
-        <div class="Home">
-            <Text
-                bgGradient='linear(to-r, #99DC94, #4a6ac2)'
-                bgClip='text'
-                fontSize='5xl'
-                fontWeight='bold'
-            >  globetrotter
-            </Text>
-            <p class="info">your AI-travel planner</p>
-            <Link as={NavLink} to='/recommend'>
-                <Button mt={4} colorScheme='blue'>adventure?</Button>
-            </Link>
-        </div>
-    );
+  useEffect(() => {
+    mapboxgl.accessToken = mapboxgl.accessToken;
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/sakula8/cligzjwqu003h01r759cf2kkb",
+      center: [-90, 90],
+      zoom: 0,
+    });
+    // Cleanup the map instance when the component is unmounted
+    return () => map.remove();
+  }, []);
+
+  return (
+    <div className="Home">
+      <div id="map"></div>
+      <Text
+        className="overlay-text"
+        bgGradient="linear(to-r, #99DC94, #4a6ac2)"
+        bgClip="text"
+        fontSize="6xl"
+        paddingBottom={500}
+      >
+        globetrotter
+      </Text>
+      <div className="button-container">
+        <Link as={NavLink} to="/recommend">
+          <Button colorScheme="teal">
+            adventure?
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
