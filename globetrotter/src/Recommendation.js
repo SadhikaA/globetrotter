ae9af78620938c384e57320b402c26f9d0eca5bd
Sadhika Akula <sadhika.akula@berkeley.edu>
2023-06-04 08:25:12 -0700

    final recommendation

--- globetrotter/src/Recommendation.js
+++ globetrotter/src/Recommendation.js
@@ -28,7 +28,6 @@ function Recommendation() {
             country: '...',
             // ... other form fields ...
         };
-
         try {
             const response = await axios.post('http://127.0.0.1:5000/api/submit-data', formData);
             console.log(response.data);  // Log the response from the backend
